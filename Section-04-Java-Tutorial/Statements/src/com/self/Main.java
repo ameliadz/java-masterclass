8fac133cc34c33834e4226ab9958773a83801ef0
Amelia Zecker <amelia.zecker@jackpinetech.com>
2021-08-10 02:09:40 -0400

    statements lesson in progress

---
+++ Section-04-Java-Tutorial/Statements/src/com/self/Main.java
@@ -0,0 +1,19 @@
+package com.self;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // declaration statement
+	    int finalScore = 50;
+	    int playerLives = 3;
+	    int playersHighScore;
+
+	    // expression statement
+        // examples: assignment expressions, method calls, object creation expressions
+        finalScore = 60;
+        playerLives--;
+
+        // control flow statement
+
+    }
+}
