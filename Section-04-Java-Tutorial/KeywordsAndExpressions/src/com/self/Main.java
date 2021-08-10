9a3a8525b65525d054dbf6ddb60bcc66fe11ecef
Amelia Zecker <amelia.zecker@jackpinetech.com>
2021-08-10 02:00:38 -0400

    next section

---
+++ Section-04-Java-Tutorial/KeywordsAndExpressions/src/com/self/Main.java
@@ -0,0 +1,14 @@
+package com.self;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // naming convention practices
+        double monthlyMilesDriven = 1000;
+        int dailyStoreCustomers = 50;
+        char lastKeyPressed = 'k';
+
+        // operator associativity example right-to-left;
+        double kilometers = 100 * 1.603944;
+    }
+}
