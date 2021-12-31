a863ac0d28f81a8eb07f264e3508f5d1d8413023
Amelia <ameliazecker@gmail.com>
2021-12-31 00:53:52 -0500

    introducing methods

---
+++ Section-04-Java-Tutorial/Methods/src/com/self/Methods.java
@@ -0,0 +1,36 @@
+package com.self;
+
+public class Methods {
+
+    public static void main(String[] args) {
+        boolean isGameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore();
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(isGameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static void calculateScore() {
+        boolean isGameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(isGameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
